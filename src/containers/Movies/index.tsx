f5b78c7df183bb7e319298921b9c2a1b746bca0e
Tailine <tailinechagas@gmail.com>
2019-01-11 08:05:28 -0300

    css modificado

--- src/containers/Movies/index.tsx
+++ src/containers/Movies/index.tsx
@@ -32,6 +32,7 @@ export class Movies extends React.Component<{}, IState> {
   public render() {
     const Card = styled.div`
       margin: 3em 0;
+      background-color: #edeeef;
 
       @media (min-width: 760px) {
         display: grid;
@@ -40,8 +41,9 @@ export class Movies extends React.Component<{}, IState> {
     `;
 
     const Cover = styled.img`
-      height: 100%;
       width: 100%;
+      max-height: 400px;
+      margin-bottom: -5px;
 
       @media (min-width: 760px) {
         min-height: 400px;
@@ -69,7 +71,7 @@ export class Movies extends React.Component<{}, IState> {
       border-radius: 50%;
       padding: 0.2em;
       position: relative;
-      top: -2em;
+      top: -3em;
       left: 0.7em;
     `;
 
@@ -83,7 +85,12 @@ export class Movies extends React.Component<{}, IState> {
       color: #eee;
     `;
 
-    const CardLink = styled(Link)`
+    const Date = styled.p`
+      color: #c4c8ce;
+      margin-left: 7em;
+    `;
+
+    const DetailsLink = styled(Link)`
       text-decoration: none;
       color: #7e7e7f;
     `;
@@ -92,19 +99,22 @@ export class Movies extends React.Component<{}, IState> {
 
     const displayMovies = () => {
       const renderMovies = movies.map(m => {
+        const date = m.release_date.split("-");
+        const formatedDate = `${date[2]}/${date[1]}/${date[0]}`;
         return (
-          <CardLink to={"/detalhes/" + m.id} key={m.id}>
+          <DetailsLink to={"/detalhes/" + m.id} key={m.id}>
             <Card key={m.id}>
               <Cover src={`https://image.tmdb.org/t/p/w400/${m.poster_path}`} />
               <Description>
                 <FilmTitle>{m.title}</FilmTitle>
+                <Date>{formatedDate}</Date>
                 <RatingContainer>
-                  <Rating>{m.vote_average * 10}</Rating>
+                  <Rating>{m.vote_average * 10}%</Rating>
                 </RatingContainer>
                 <Sinopse>{m.overview}</Sinopse>
               </Description>
             </Card>
-          </CardLink>
+          </DetailsLink>
         );
       });
       return renderMovies;
